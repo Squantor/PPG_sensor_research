d47c38dba73509580011d4aa7384ee71666165f3
Squantor <squantmuts@gmail.com>
2020-08-02 22:20:24 +0200

    LED control functional at 30HZ

--- software/PPG_sensor_V000/src/main.cpp
+++ software/PPG_sensor_V000/src/main.cpp
@@ -34,20 +34,46 @@ Simple uart example
 #include <time_delay.hpp>
 #include <time_interval.hpp>
 
+#define PPG_SENSOR_FREQ     (1000000)
+#define PPG_SENSOR_DUTY     (PPG_SENSOR_FREQ / 1000)
+
+
 void ppgSensorSetup(void)
 {
     ClockEnablePeriphClock(SYSCTL_CLOCK_SWM);
     ClockEnablePeriphClock(SYSCTL_CLOCK_IOCON);
     IoconPinSetMode(LPC_IOCON, IOCON_LED_CTRL, PIN_MODE_INACTIVE);
+    SwmMovablePinAssign(SWM_CTOUT_0_O, PIN_LED_CTRL);
     ClockDisablePeriphClock(SYSCTL_CLOCK_IOCON);
     ClockDisablePeriphClock(SYSCTL_CLOCK_SWM);
-    // connect SCT pins
+    
+    SctInit(LPC_SCT);
+
+    SctSetConfig(LPC_SCT, SCT_CONFIG_32BIT_COUNTER | SCT_CONFIG_AUTOLIMIT_U);
+
+    SctMatchU(LPC_SCT, SCT_MATCH_0, PPG_SENSOR_FREQ);
+    SctMatchReloadU(LPC_SCT, SCT_MATCH_0, PPG_SENSOR_FREQ);
+    SctMatchU(LPC_SCT, SCT_MATCH_1, PPG_SENSOR_DUTY);
+    SctMatchReloadU(LPC_SCT, SCT_MATCH_1, PPG_SENSOR_DUTY);
+
+    SctSetEventStateMask(LPC_SCT, SCT_EVENT_0_VAL, SCT_STATE_0_BIT | SCT_STATE_1_BIT);
+    SctSetEventControl(LPC_SCT, SCT_EVENT_0_VAL, SCT_EV_CTRL_MATCHSEL(SCT_MATCH_0) | SCT_EV_CTRL_COMBMODE(SCT_COMBMODE_MATCH));
+    SctSetEventStateMask(LPC_SCT, SCT_EVENT_1_VAL, SCT_STATE_0_BIT | SCT_STATE_1_BIT);
+    SctSetEventControl(LPC_SCT, SCT_EVENT_1_VAL, SCT_EV_CTRL_MATCHSEL(SCT_MATCH_1) | SCT_EV_CTRL_COMBMODE(SCT_COMBMODE_MATCH));
+
+    SctOutputSet(LPC_SCT, SCT_OUTPUT_0_VALUE, SCT_EVENT_0_BIT);
+    SctOutputClear(LPC_SCT, SCT_OUTPUT_0_VALUE, SCT_EVENT_1_BIT);
+
+    SctOutput(LPC_SCT, SCT_OUTPUT_STATE(SCT_OUTPUT_0_VALUE, 1));
+
+    SctClearControl(LPC_SCT, SCT_CTRL_HALT_U);
 }
 
 int main()
 {
     boardInit();
     dsPuts(&streamUart, strHello);
+    ppgSensorSetup();
     while (1) {
     }
 }
