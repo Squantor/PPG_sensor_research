ab570a05ff394e664fd93dc755c231e931fe7db0
Squantor <squantmuts@gmail.com>
2021-01-26 21:20:55 +0100

    updated to newer buildsystem

--- software/PPG_sensor_V100/PPG_sensor_V100.mk
+++ software/PPG_sensor_V100/PPG_sensor_V100.mk
@@ -1,35 +1,20 @@
-# MIT License
-
-# Copyright (c) 2020 Bart Bilos
-
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
-
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
+# SPDX-License-Identifier: MIT
+# Copyright (c) 2021 Bart Bilos
+# For conditions of distribution and use, see LICENSE file
 
 # project settings
 #
-# Version: 20200625
+# Version: 20210126
 
 # includes of additional libraries
+
+include libMcuLL/libMcuLL.mk
 include squantorLibC/squantorLibC.mk
+include libEmbedded/libEmbedded.mk
 include squantorLibEmbeddedC/squantorLibEmbeddedC.mk
 
 # project settings
-MCU = LPC812M101JDH20
+MCU = LPC812M101DH20
 TARGET = MCU
 BOARD = PPG_sensor_V100
 
@@ -45,6 +30,6 @@ $(PROJECT)/src/$(BOARD).cpp \
 $(PROJECT)/src/main.cpp
 
 LIBS +=
-INCLUDES += -Icommon/inc -IlibMcuLL/inc -I$(PROJECT)/inc
+INCLUDES += -Icommon/inc -I$(PROJECT)/inc
 DEFINES += -D"$(BOARD)"
 
