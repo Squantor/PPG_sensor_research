42220f91dd831a55be44baa657efa8af2c5bca64
Squantor <squantmuts@gmail.com>
2021-02-14 16:33:37 +0100

    changed timing and some names

--- software/PPG_sensor_V120/inc/PPG_sensor_adc.hpp
+++ software/PPG_sensor_V120/inc/PPG_sensor_adc.hpp
@@ -10,9 +10,9 @@
 #include <stdint.h>
 
 #define PPG_SENSOR_FREQ     (1000000)
-#define PPG_SENSOR_ON       (300000)
+#define PPG_SENSOR_LED_ON   (60000)
 #define PPG_SENSOR_IRESET   (1000)
-#define PPG_ADC_RATE        (100000u)
+#define PPG_ADC_RATE        (1000000u)
 
 void ppgSensorSetup(void);
 
